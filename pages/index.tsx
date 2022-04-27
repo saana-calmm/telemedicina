82d0b229ffc1bd8f728bc9f1404b1e440a5835df
Jesusebz <jesusebzcp19@gmail.com>
2022-04-26 23:17:24 -0500

    add resposnive

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { query } = router;
   const { id } = query;
+
+  useEffect(() => {
+    window.location.replace("https://www.saana.com.co/");
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
